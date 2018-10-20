640cecef35b3c7a41f9f79ead4da57ef23870723
Kevin Hart <kevin.kevinhart@gmail.com>
2018-10-20 11:05:11 -0700

    Finished tutorial part 2

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use tcod::console::*;
 use tcod::colors;
 use tcod::colors::Color;
 
+#[derive(Debug)]
 struct Object {
     x: i32,
     y: i32,
@@ -21,11 +22,16 @@ impl Object {
         }
     }
 
-    pub fn move_by(&mut self, dx: i32, dy: i32) {
+    pub fn move_by(&mut self, map: &Map, dx: i32, dy: i32) {
         // move by the given amount
-        // TODO:  Collision detection and range check
-        self.x += dx;
-        self.y += dy;
+        let next_x = self.x + dx;
+        let next_y = self.y + dy;
+        if 0 <= next_x && next_x < MAP_WIDTH && 0 <= next_y && next_y < MAP_HEIGHT {
+            if !map[next_x as usize][next_y as usize].blocked {
+                self.x += dx;
+                self.y += dy;
+            }
+        }
     }
 
     pub fn draw(&self, con: &mut Console) {
@@ -38,11 +44,47 @@ impl Object {
     }
 }
 
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+
+const COLOR_DARK_WALL: Color = Color { r:0, g:0, b:100 };
+const COLOR_DARK_GROUND: Color = Color { r:50, g:50, b:150 };
+
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile {blocked: false, block_sight: false}
+    }
+
+    pub fn wall() -> Self {
+        Tile {blocked: true, block_sight: true}
+    }
+
+    pub fn new(blocked: bool, block_sight: bool) -> Self {
+        Tile {
+            blocked: blocked,
+            block_sight: block_sight,
+        }
+    }
+}
+
+type Map = Vec<Vec<Tile>>;
+
+fn make_map() -> Map {
+    let map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+    map
+}
+
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 const LIMIT_FPS: i32 = 20;
 
-fn handle_keys(root: &mut Root, player: &mut Object) -> bool {
+fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
 
@@ -55,14 +97,32 @@ fn handle_keys(root: &mut Root, player: &mut Object) -> bool {
         Key { code: Escape, .. } => return true,
 
         // Movement Keys
-        Key { code: Up, .. } => player.y -= 1,
-        Key { code: Down, .. } => player.y += 1,
-        Key { code: Left, .. } => player.x -= 1,
-        Key { code: Right, .. } => player.x += 1,
+        Key { code: Up, .. } => player.move_by(map, 0, -1),
+        Key { code: Down, .. } => player.move_by(map, 0, 1),
+        Key { code: Left, .. } => player.move_by(map, -1, 0),
+        Key { code: Right, .. } => player.move_by(map, 1, 0),
 
         _ => {},
     }
     false
+}
+
+fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
+    for object in objects {
+        object.draw(con);
+    }
+
+    for x in 0..MAP_WIDTH {
+        for y in 0..MAP_HEIGHT {
+            if map[x as usize][y as usize].block_sight {
+                con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+        }
+    }
+
+    blit(con, (0, 0), (MAP_WIDTH, MAP_HEIGHT), root, (0, 0), 1.0, 1.0);
 }
 
 fn main() {
@@ -73,27 +133,27 @@ fn main() {
         .title("Rust/libtcod tutorial")
         .init();
 
-    let mut con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
-    let mut player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', colors::WHITE);
-    let mut npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', colors::WHITE);
+    let mut con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
+    let mut map = make_map();
+    map[30][22] = Tile::wall();
+    map[50][22] = Tile::wall();
+
+    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', colors::WHITE);
+    let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', colors::WHITE);
     let mut objects = [player, npc];
 
     tcod::system::set_fps(LIMIT_FPS);
 
 
     while !root.window_closed() {
-        for object in &mut objects {
-            object.draw(&mut con);
-        }
-
-        blit(&mut con, (0, 0), (SCREEN_WIDTH, SCREEN_HEIGHT), &mut root, (0, 0), 1.0, 1.0);
+        render_all(&mut root, &mut con, &objects, &map);
         root.flush();
 
         for object in &mut objects {
             object.clear(&mut con);
         }
 
-        if handle_keys(&mut root, &mut objects[0]) {
+        if handle_keys(&mut root, &mut objects[0], &map) {
             break;
         }
     }
